f6112079e5572b1dcc9fc60e503b501d85735792
caio546 <caioom00@gmail.com>
2020-04-27 19:38:38 -0300

    Finalizacao do app

--- src/components/LazyImage/index.js
+++ src/components/LazyImage/index.js
@@ -9,15 +9,18 @@ export default function LazyImage({
   smallSource,
   source,
   aspectRatio,
+  shouldLoad,
 }) {
   const opacity = new Animated.Value(0);
   const [loaded, setLoaded] = useState(false); 
 
   useEffect(() => {
-    setTimeout(() => {
-      setLoaded(true);
-    }, 1000);
-  }, []);
+    if (shouldLoad) {
+      setTimeout(() => {
+        setLoaded(true);
+      }, 1000);
+    }
+  }, [shouldLoad]);
 
   function handleAnimate() {
     Animated.timing(opacity, {
